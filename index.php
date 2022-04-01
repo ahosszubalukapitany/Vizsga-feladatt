e1ac41a1d1b8de7ec934e58ef34d1a57005eaa6e
ahosszubalukapitany <100533537+ahosszubalukapitany@users.noreply.github.com>
2022-04-01 23:13:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
+  <title>hmmm</title>
+  <link rel="stylesheet" type="text/css"  href="style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+  
 </head>
 <body>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">autokisokos</a>
+<div class="container"> 
+
+  <div class= "login-box">
+
+    <div class= "row"> 
+
+      <div class= "col-md-6 login-left"> 
+      <h2>bejelentkezés</h2>
+      <form action="Vw.php" method="post">
+         <div class="form-grup">
+           <label>Felhasználo név</label>
+           <input type="text" name="user" class="form-control" required>
+         </div>
+
+         <div class="form-grup">
+           <label>jelszo </label>
+           <input type="password" name="password" class="form-control" required>
+         </div>
+         
+         <button type="submit" class="btn btn-primary"> bejelentkezé</button>
+        </form>
+
+      </div>
+
+      <div class= "col-md-6 login-right"> 
+      <h2>Regisztrácio</h2>
+      <form action="register.php" method="post"></form>
+         <div class="form-grup">
+           <label>Felhasználo név</label>
+           <input type="text" name="user" class="form-control" required>
+         </div>
+
+         <div class="form-grup">
+           <label>jelszo </label>
+           <input type="password" name="password" class="form-control" required>
+         </div>
+         
+         <button type="submit" class="btn btn-primary"> regisztrálok</button>
+
+      </div>
+
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="Vw.php">Vw</a></li>
-      <li><a href="opel.php"> Opel</a></li>
-      <li><a href="renault.php"> Renault</a></li>
-	    <li><a href="suzuki.php">Suzuki</a></li>
-      <li><a href="register.php">Regisztrácio</a></li>
 
-	  
-
-    </ul>
-  </div>
-</nav>
-  
-<div class="container">
-  <h1>Udv !</h1>
-  <h2>Ez az oldal PÁR  auto tipusoknak a tipus hibáit illetve a kocsirol adatokat fog be mutatni  </h2>
+ </div>  
 
 </div>
 
